ce8b2ebcb3e4350d9ebcca4ecf74c21844b43cac
Mozza <andrewmichaelmorris1999@gmail.com>
2024-03-26 21:26:59 +0000

    Changes
    1: artist listing fixed to now list groups as well as soloists
    
    2: translations added for characters likely to be found in song names (for URL formatting purposes)
    
    3: bypass created for youtube, simply add API_KEY_4, API_KEY_5 etc to config.json to add new API keys. MAX IS 20.
    
    4: duplicate URL now adds +1 to the end, meaning if there are 3 duplicates you will have original, original_1, original_2.  This is due to the way the website handles this
    
    5: Added more rules for replacing characters to help match kpopping's formatting
    
    6: Output to excel created
    
    CODE COMMENTED OUT FOR DEVELOPMENT PURPOSES AT THIS TIME

--- main/main.py
+++ main/main.py
@@ -6,6 +6,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+# Converting language to the "sound" (ie Chinese > Chinese Pinyin)
+from pypinyin import lazy_pinyin, Style
+import pykakasi
+from hangul_romanize import Transliter
+from hangul_romanize.rule import academic
+import pythainlp
 # other tools
 import re
 import time
@@ -14,15 +20,20 @@ import sys
 # YouTube API
 import json
 import googleapiclient.discovery
+import googleapiclient.errors as google_error
+# Output to EXCEL
+import xlsxwriter
+import datetime
+import time
+
+transliter = Transliter(academic)
 
 # API info
 api_service_name = 'youtube'
 api_version = 'v3'
 with open('config.json') as f:
     config = json.load(f)
-API_KEY = config['API_KEY']
-youtube = googleapiclient.discovery.build(
-    api_service_name, api_version, developerKey=API_KEY)
+
 
 # Create a list for channel ID's
 cid_list = []
@@ -31,13 +42,13 @@ cid_list = []
 def db_init(chosen_year):
     con = sqlite3.connect('kp_albums.db')
     cur = con.cursor()
-    # try:
-    #    cur.execute(f'DROP TABLE IF EXISTS y{chosen_year}')
-    # except sqlite3.OperationalError:
-    #    pass
+   # try:
+   #     cur.execute(f'DROP TABLE IF EXISTS y{chosen_year}')
+   # except sqlite3.OperationalError:
+   #     pass
     print(f'y{chosen_year}')
-    # cur.execute(f"CREATE TABLE y{chosen_year}(artist, album, songs, url, mv, mvverified)")
-    # con.commit()
+   # cur.execute(f"CREATE TABLE y{chosen_year}(artist, album, songs, url, mv, mvverified)")
+   # con.commit()
     return con, cur
 
 
@@ -48,7 +59,7 @@ def main(kpop_year):
     # on webpage, find all artists and albums in the list
     artists = driver.find_elements(By.CSS_SELECTOR, 'a[href*="/profiles/idol"]')
     albums = driver.find_elements(By.CSS_SELECTOR, 'a[href*="/musicalbum"]')
-
+    artist_group = driver.find_elements(By.CSS_SELECTOR, 'a[href*="/profiles/group"]')
     # create artists and albums lists
     artists_list = []
     albums_list_raw = []
@@ -56,13 +67,15 @@ def main(kpop_year):
     # append lists with text from previous elements
     for artist in artists:
         artists_list.append(artist.text)
-
+    for artist in artist_group:
+        artists_list.append(artist.text)
     for album in albums:
         albums_list_raw.append(album.text)
         # edit list to match the formatting of kpopping links
         albums_list1 = [f"{kpop_year}-" + item.replace(" ", "-") for item in albums_list_raw]
         # does NOT remove - (hyphens)
-        albums_list = [remove_special_characters(item) for item in albums_list1]
+        albums_list2 = [translate_symbol(item) for item in albums_list1]
+        albums_list = [remove_special_characters(item) for item in albums_list2]
 
         # error can be ignored
     merged_list = list(zip(artists_list, albums_list))
@@ -73,12 +86,18 @@ def main(kpop_year):
             merged_dict[artist] = [album]
         else:
             merged_dict[artist].append(album)
-
+    attempted_urls = []
+    it_num = 1
     for artist, album in merged_dict.items():
         print(artist)
         for f in album:
+            if f'https://kpopping.com/musicalbum/{f}'.lower() in attempted_urls:
+                it_num += 1
+                f = f'https://kpopping.com/musicalbum/{f}{it_num}'
             try:
                 driver.get(f'https://kpopping.com/musicalbum/{f}')
+                attempted_urls.append(f'https://kpopping.com/musicalbum/{f}'.lower())
+                print(f'https://kpopping.com/musicalbum/{f}')
             except TimeoutException:
                 attempt = 0
                 while True:
@@ -89,7 +108,7 @@ def main(kpop_year):
                         break
                     except TimeoutException:
                         if attempt == 6:
-                            print('TIMEDOUT. 6 ATTEMPTS MADE OVER 3 MINUTES.')
+                            print('TIMED OUT. 6 ATTEMPTS MADE OVER 3 MINUTES.')
                             driver.quit()
                             quit()
             try:
@@ -125,16 +144,73 @@ def remove_special_characters(s):
     # Replace spaces with hyphens - this is to try to get around the issue with ' in album names
     # had to add more as some were coming out with -- and --- in the names, this has fixed all issues of this kind
     # SO FAR
-    s = s.replace("'", " ")
-    s = s.replace(" ", "-")
-    s = s.replace(".", "-")
-    s = s.replace("---", "-")
-    s = s.replace("--", "-")
-    # Use regular expression to remove special characters, excluding apostrophes in words
-    return re.sub(r'[^a-zA-Z0-9\s-]|(?<=\w)\'(?=\w)', '', s)
+    pattern = r'[\u2665\(),!]'
+    s = re.sub(pattern, '', s)
+    replace_list_space = ["'"]
+    replace_list_dash = [' ', '.', '_', '+', '&', 'ː', '=', '÷', '/', '#', '?', '...', '’', '…', '---', '--']
+    replace_list_empty = [':', '[', ']', '"']
+    s = s.replace(':)', '2')
+    s = s.replace('Ø', 'O')
+    for i in replace_list_space:
+        s = s.replace(i, ' ')
+    for i in replace_list_empty:
+        s = s.replace(i, '')
+    for i in replace_list_dash:
+        s = s.replace(i, '-')
+    # Removes - if at the end of string
+    s = re.sub(r'-\Z', '', s)
+    s = s.replace('Ⅱ', 'II')
+    s = s.replace('Ⅲ', 'III')
+    s = s.replace('ᐸ', '<')
+    s = s.replace('ᐳ', '>')
+    s = s.replace('<', '%E1%90%B8')
+    s = s.replace('>', '%E1%90%B3')
+    # Exception due to inconsistency from kpopping
+    if s == ('https://kpopping.com/musicalbum/2023-The-Seasons-VolII-10-%E1%90%B8AKMU-s-Long-Day-Long-'
+             'Night%E1%90%B3-Re-Wake-x-Chuu'):
+        s = s.replace('%E1%90%B8', '')
+        s = s.replace('%E1%90%B3', '')
+    return s
+
+
+def translate_symbol(s):
+    chinese_chars = re.findall(r'[\u4e00-\u9fff]+', s)
+    for chinese_char in chinese_chars:
+        pinyin_char = ''.join(lazy_pinyin(chinese_char, style=Style.NORMAL))
+        s = s.replace(chinese_char, pinyin_char)
+        s = s.replace('[', '-')
+        s = s.replace(']', '')
+    japanese_chars = re.findall(r'[\u3040-\u30FF\u31F0-\u31FF\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]+', s)
+    for japanese_char in japanese_chars:
+        kks = pykakasi.kakasi()
+        result = kks.convert(japanese_char)
+        for item in result:
+            romaji_char = ''.join(item['hepburn'])
+            s = s.replace(japanese_char, romaji_char)
+        s = s.replace('[', '-')
+        s = s.replace(']', '')
+    korean_chars = re.findall(r'[\uac00-\ud7af\u1100-\u1112\u1161-\u1175\u11A8-\u11C2]+', s)
+    for korean_char in korean_chars:
+        romanized_char = ''.join(transliter.translit(korean_char))
+        s = s.replace(korean_char, romanized_char)
+        s = s.replace('[', '-')
+        s = s.replace(']', '')
+    thai_chars = re.findall(r'[\u0e00-\u0e7f]+', s)
+    for thai_char in thai_chars:
+        thairom_char = ''.join(pythainlp.romanize(thai_char))
+        s = s.replace(thai_char, thairom_char)
+        s = s.replace('[', '-')
+        s = s.replace(']', '')
+    return s
 
 
 def music_video():
+    attempt_number = 0
+    max_api = range(1, 20)
+    api_raw = generate_api_config(max_api)
+    api_key = api_config(attempt_number, api_raw)
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=api_key)
     with open('json_response.txt', 'w+', encoding='utf-8'):
         # used to create fresh text file before continuing
         pass
@@ -151,12 +227,93 @@ def music_video():
                 order='relevance',
                 type='video'
             )
-            response = request.execute()
+            while True:
+                try:
+                    response = api_response(request)
+                    break
+                except google_error.HttpError:
+                    attempt_number += 1
+                    api_key = api_config(attempt_number, api_raw)
+                    youtube = googleapiclient.discovery.build(
+                        api_service_name, api_version, developerKey=api_key)
+                    try:
+                        request = youtube.search().list(
+                            part='snippet',
+                            q=f'{mv_res} {artist}',
+                            maxResults=10,
+                            order='relevance',
+                            type='video'
+                        )
+                        if api_key == 0:
+                            print('No more API keys available.. Waiting until quota reset')
+                            response = quota_wait(request)
+                        else:
+                            response = api_response(request)
+                        break
+                    except google_error.HttpError:
+                        attempt_number += 1
+                        api_key = api_config(attempt_number, api_raw)
+                        youtube = googleapiclient.discovery.build(
+                            api_service_name, api_version, developerKey=api_key)
+                    request = youtube.search().list(
+                        part='snippet',
+                        q=f'{mv_res} {artist}',
+                        maxResults=10,
+                        order='relevance',
+                        type='video'
+                    )
+                    if api_key == 0:
+                        print('No more API keys available.. Waiting until quota reset')
+                        response = quota_wait(request)
+                    else:
+                        response = quota_wait(request)
+                    break
+
             f.write(f'{response}\n\n')
             format_response = convert_human_readable(response)
             with open('format_response.txt', 'a', encoding='utf-8') as jf:
                 jf.write(f'{format_response}\n\n')
             mv_database(format_response, song, artist)
+
+
+def generate_api_config(max_api):
+    api_dict = {}
+    for i in max_api:
+        api_dict[f'api_{i}'] = f'API_KEY_{i}'
+    return api_dict
+
+
+def api_config(key_number, api_dict):
+    key_number += 1
+    print(key_number)
+    api_raw = api_dict[f'api_{key_number}']
+    try:
+        api_key = config[api_raw]
+    except KeyError:
+        api_key = 0
+    return api_key
+
+
+def bypass_quota(key):
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=key)
+    print(key)
+    return youtube
+
+
+def api_response(request):
+    response = request.execute()
+    return response
+
+
+def quota_wait(request):
+    now = datetime.datetime.now()
+    midnight = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(days=1)
+    time_until_midnight = midnight - now
+    print(f'Waiting until midnight UK/LONDON - {time_until_midnight.seconds} seconds')
+    time.sleep(time_until_midnight.seconds)
+    response = api_response(request)
+    return response
 
 
 def convert_human_readable(data, indent=0):
@@ -345,8 +502,86 @@ def manual_verify(cid):
         manual_verify(cid)
 
 
-# add function to verify channel is legit. if it is not found in verified_channels.db then ask if it should be
-# verified or not. This will add a 0 (no) or 1 (yes) tag to a "mvverified" column in the original database
+def output_excel():
+    workbook = xlsxwriter.Workbook(f"output/{datetime.date.today().strftime('%y%m%d')}_{datetime.datetime.now()
+                                   .strftime('%H%M%S')}.xlsx")
+    worksheet = workbook.add_worksheet(f'KPOP_{chosen_year_q}')
+
+    row = 0
+    col = 0
+
+    artists = cur.execute("""SELECT artist FROM mv""").fetchall()
+    print(artists)
+    already_read = set()
+    worksheet.write(row, col, 'ARTIST')
+    worksheet.write(row, col + 1, 'SONG')
+    worksheet.write(row, col + 2, 'ALBUM')
+    worksheet.write(row, col + 3, 'MUSIC VIDEO')
+    worksheet.write(row, col + 4, 'DANCE PRACTICE')
+    worksheet.write(row, col + 5, 'PERFORMANCE/STAGE')
+    worksheet.write(row, col + 6, 'TEASER')
+    worksheet.write(row, col + 7, 'REACTION')
+    worksheet.write(row, col + 8, 'LYRICS')
+    worksheet.write(row, col + 9, 'OTHER')
+    row += 1
+    for artist in artists:
+        if artist not in already_read:
+            already_read.add(artist)
+            artist_list = list(artist)
+            artist = str(artist_list[0])
+            songs = cur.execute(f"""SELECT songs FROM y{chosen_year_q} WHERE artist=?""", (artist,)).fetchall()
+            already_read_song = set()
+            for ii in songs:
+                if ii not in already_read_song:
+                    print(artist)
+                    song_list = list(ii)
+                    sel_song = str(song_list[0])
+                    worksheet.write(row, col, artist)
+                    worksheet.write(row, col+1, sel_song)
+                    already_read_song.add(ii)
+                    video_res = cur.execute(f"""SELECT video FROM mv WHERE song=?""", (ii)).fetchall()
+                    video_list = list(video_res)
+                    mv_list = []
+                    dance_list = []
+                    performance_list = []
+                    teaser_list = []
+                    reaction_list = []
+                    lyric_list = []
+                    other_list = []
+                    for vid_sql in video_list:
+                        pattern = r'[\'(),]'
+                        vid = re.sub(pattern, '', str(vid_sql))
+                        vid_type = cur.execute("""SELECT vid_type FROM video_type WHERE vid_id=?""", (
+                            f'https://www.youtube.com/watch?v={vid}',)).fetchone()
+                        pattern = r'[\'(),]'
+                        vid_type = re.sub(pattern, '', str(vid_type))
+                        print(f'https://www.youtube.com/watch?v={vid}')
+
+                        if vid_type == 'mv':
+                            mv_list.append(f' | https://www.youtube.com/watch?v={vid}')
+                        elif vid_type == 'dance':
+                            dance_list.append(f' | https://www.youtube.com/watch?v={vid}')
+                        elif vid_type == 'performance':
+                            performance_list.append(f' | https://www.youtube.com/watch?v={vid}')
+                        elif vid_type == 'teaser':
+                            teaser_list.append(f' | https://www.youtube.com/watch?v={vid}')
+                        elif vid_type == 'reaction':
+                            reaction_list.append(f' | https://www.youtube.com/watch?v={vid}')
+                        elif vid_type == 'lyric':
+                            lyric_list.append(f' | https://www.youtube.com/watch?v={vid}')
+                        elif vid_type == 'other':
+                            other_list.append(f' | https://www.youtube.com/watch?v={vid}')
+                        else:
+                            print(f'N/A - {artist} potentially has no videos found. Please check output.')
+                        type_list = [mv_list, dance_list, performance_list, teaser_list, reaction_list, lyric_list,
+                                     other_list]
+                        col_num = 3
+                        for i in type_list:
+                            format_list = ''.join(i)
+                            worksheet.write(row, col+col_num, format_list)
+                            col_num += 1
+                    row += 1
+    workbook.close()
 
 
 if __name__ == '__main__':
@@ -362,10 +597,10 @@ if __name__ == '__main__':
         kp_db.commit()
     except sqlite3.OperationalError:
         pass
-    cur.execute("DROP TABLE IF EXISTS video_type")
-    kp_db.commit()
-    cur.execute(f"CREATE TABLE video_type(name, vid_id, vid_type)")
-    kp_db.commit()
+    #cur.execute("DROP TABLE IF EXISTS video_type")
+    #kp_db.commit()
+    #cur.execute(f"CREATE TABLE video_type(name, vid_id, vid_type)")
+    #kp_db.commit()
 
     # Setup geckodriver (Firefox)
     print('Launching Firefox via Geckodriver in HEADLESS mode..')
@@ -375,11 +610,12 @@ if __name__ == '__main__':
     service = Service(executable_path=geckodriver_path)
     driver = webdriver.Firefox(options=options, service=service)
     print(f'Checking kpopping list for year {chosen_year_q}..')
-    # main(chosen_year_q)
+    #main(chosen_year_q)
     print('List created. Checking music videos..')
     music_video()
     print('Data grab complete. Checking channel verification status.. (May take a while.. '
           'Approx. 6-7 seconds per video)')
-    code_timer_start = time.time()
-    channel_verified()
-    driver.quit()
+    #code_timer_start = time.time()
+    #channel_verified()
+    #driver.quit()
+    output_excel()
